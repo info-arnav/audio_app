9a01d56941c9b15c059bff3bd92bb20388dbcf62
Arnav Gupta <info@arnavgupta.net>
2022-11-09 00:02:33 +0530

    syncng

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # audio_app
-"# audio_app" 
+
+# Audio App
+
+## Onedrive Link
+
+https://1drv.ms/u/s!AlfH79fKM_bvisVe_wk2c3V6TG5j0Q?e=KbD4M0
--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const path = require("path");
+const NormalizeVolume = require("normalize-volume");
 
 const app = express();
 const PORT = 3000;
+let options = {
+  normalize: true,
+  waveform: { width: 1400, height: 225 },
+  ffmpeg_bin: "./ffmpeg.exe",
+  convert_bin: "./convert.exe",
+};
+
+// requirment
+// {
+//     "video" : file_in_base64_or_url
+// }
 
 app.get("/", (req, res) => {
+  NormalizeVolume(req.body.video, ".\normalized.mp4", options)
+    .then((result) => {
+      res.status(200);
+      res.sendFile(path.join(__dirname, "./normalized.mp4"));
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send("Some error occured");
+    });
+});
+
+app.get("/test", (req, res) => {
   res.status(200);
-  res.sendFile(path.join(__dirname, "./test.mp4"));
+  res.send("Working");
 });
 
 app.listen(PORT, (error, success) => {
